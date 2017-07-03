4fd303b1e33caf16a0174520a6674326a8e67c50
lcyzgdy <MyGuanDY@outlook.com>
2017-07-03 23:05:31 +0800

    Update the script to control a 2D car with WhellJoint2D.

---
+++ Assets/Script/IsOnGround.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsOnGround: MonoBehaviour
+{
+	bool isGround;
+	Vector2 contactPoint;
+	[SerializeField] LayerMask whatIsGround;
+	CircleCollider2D circle;
+
+	public bool IsGround
+	{
+		get
+		{
+			return isGround;
+		}
+	}
+
+	public Vector2 ContactPoint
+	{
+		get
+		{
+			return contactPoint;
+		}
+	}
+
+	private void Start()
+	{
+		circle = GetComponent<CircleCollider2D>();
+	}
+
+	private void Update()
+	{
+		//Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, k_GroundedRadius, whatIsGround);
+		var collisions = Physics2D.CircleCastAll(transform.position, circle.radius, Vector2.zero, circle.radius + 0.1f, whatIsGround);
+		if (collisions.Length != 0) isGround = true;
+		else isGround = false;
+		for (int i = 0; i < collisions.Length; i++)
+		{
+			//Debug.DrawRay(collisions[i].point, Vector3.down, Color.red);
+			contactPoint = collisions[i].point;
+		}
+	}
+	/*
+	private void OnCollisionStay2D(Collision2D collision)
+	{
+		contactPoint = collision.contacts[0].point;
+		isGround = true;
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		isGround = false;
+	}*/
+}
---
+++ Assets/Script/MyControlCarWithWheel.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyControlCarWithWheel : MonoBehaviour
+{
+	[SerializeField] private WheelJoint2D[] wheels;
+	[SerializeField] private float maxSpeed;
+	[SerializeField] private float acceleration;
+	private Vector2 groundDirection;
+	private new Rigidbody2D[] rigidbody;
+	// Use this for initialization
+	void Start()
+	{
+		rigidbody = new Rigidbody2D[wheels.Length];
+		for (var i = 0; i < wheels.Length; i++)
+		{
+			rigidbody[i] = wheels[i].GetComponent<Rigidbody2D>();
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.anyKeyDown)
+		{
+			if (Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				for (var i = 0; i < wheels.Length; i++)
+				{
+					wheels[i].useMotor = true;
+					wheels[i].motor = new JointMotor2D()
+					{
+						motorSpeed = -rigidbody[i].angularVelocity,
+						maxMotorTorque = wheels[i].motor.maxMotorTorque
+					};
+				}
+			}
+			if (Input.GetKeyDown(KeyCode.RightArrow))
+			{
+				for (var i = 0; i < wheels.Length; i++)
+				{
+					wheels[i].useMotor = true;
+					wheels[i].motor = new JointMotor2D()
+					{
+						motorSpeed = -rigidbody[i].angularVelocity,
+						maxMotorTorque = wheels[i].motor.maxMotorTorque
+					};
+				}
+			}
+		}
+		if (Input.anyKey)
+		{
+			if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				foreach (var i in wheels)
+				{
+					i.useMotor = true;
+					i.motor = new JointMotor2D()
+					{
+						motorSpeed = (i.motor.motorSpeed - acceleration) > -maxSpeed ? i.motor.motorSpeed - acceleration : -maxSpeed,
+						maxMotorTorque = i.motor.maxMotorTorque
+					};
+				}
+			}
+			if (Input.GetKey(KeyCode.RightArrow))
+			{
+				foreach (var i in wheels)
+				{
+					i.useMotor = true;
+					i.motor = new JointMotor2D()
+					{
+						motorSpeed = (i.motor.motorSpeed + acceleration) < maxSpeed ? i.motor.motorSpeed + acceleration : maxSpeed,
+						maxMotorTorque = i.motor.maxMotorTorque
+					};
+				}
+			}
+		}
+		else
+		{
+			foreach (var i in wheels)
+			{
+				i.useMotor = false;
+				if (i.motor.motorSpeed > 0)
+				{
+					i.motor = new JointMotor2D()
+					{
+						motorSpeed = (i.motor.motorSpeed - acceleration / 2) > 0f ? i.motor.motorSpeed - acceleration / 2 : 0f,
+						maxMotorTorque = i.motor.maxMotorTorque
+					};
+				}
+				else if (i.motor.motorSpeed < 0)
+				{
+					i.motor = new JointMotor2D()
+					{
+						motorSpeed = (i.motor.motorSpeed + acceleration / 2) < 0f ? i.motor.motorSpeed + acceleration / 2 : 0f,
+						maxMotorTorque = i.motor.maxMotorTorque
+					};
+				}
+			}
+		}
+	}
+}
