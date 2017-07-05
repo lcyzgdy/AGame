630a5dede9e9df55e04aa19f77b3ad95c3a30764
lcyzgdy <MyGuanDY@outlook.com>
2017-07-05 22:37:32 +0800

    Clear wrong code

--- Assets/Script/IsOnGround.cs
+++ Assets/Script/IsOnGround.cs
@@ -32,25 +32,12 @@ public class IsOnGround: MonoBehaviour
 
 	private void Update()
 	{
-		//Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, k_GroundedRadius, whatIsGround);
 		var collisions = Physics2D.CircleCastAll(transform.position, circle.radius, Vector2.zero, circle.radius + 0.1f, whatIsGround);
 		if (collisions.Length != 0) isGround = true;
 		else isGround = false;
 		for (int i = 0; i < collisions.Length; i++)
 		{
-			//Debug.DrawRay(collisions[i].point, Vector3.down, Color.red);
 			contactPoint = collisions[i].point;
 		}
 	}
-	/*
-	private void OnCollisionStay2D(Collision2D collision)
-	{
-		contactPoint = collision.contacts[0].point;
-		isGround = true;
-	}
-
-	private void OnCollisionExit2D(Collision2D collision)
-	{
-		isGround = false;
-	}*/
 }
