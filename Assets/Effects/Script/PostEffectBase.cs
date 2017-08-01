d780cdd89adbd5aed9861fe5047b529a71711487
lcyzgdy <MyGuanDY@outlook.com>
2017-08-01 23:24:31 +0800

    Use the shader to generate image effect

---
+++ Assets/Effects/Script/PostEffectBase.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class PostEffectBase : MonoBehaviour
+{
+	public Shader shader = null;
+	private Material myMaterial = null;
+
+	public Material MyMaterial
+	{
+		get
+		{
+			if (myMaterial == null)
+			{
+				myMaterial = GenerateMaterial(shader);
+			}
+
+			return myMaterial;
+		}
+	}
+
+	//根据shader创建用于屏幕特效的材质  
+	protected Material GenerateMaterial(Shader shader)
+	{
+		if (shader == null)
+		{
+			return null;
+		}
+		//需要判断shader是否支持  
+		if (shader.isSupported == false)
+		{
+			return null;
+		}
+
+		Material material = new Material(shader)
+		{
+			hideFlags = HideFlags.DontSave
+		};
+		if (material)
+		{
+			return material;
+		}
+
+		return null;
+	}
+}
--- Assets/Script/Load2NextScene.cs
+++ Assets/Script/Load2NextScene.cs
@@ -1,23 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Load2NextScene : MonoBehaviour
 {
-
-	// Use this for initialization
-	void Start()
+	public async void Jump2NextLevel()
 	{
+		GetComponent<Button>().enabled = false;
+		await Task.Run(() =>
+		{
 
-	}
-
-	// Update is called once per frame
-	void Update()
-	{
-
-	}
-
-	void Jump2NextLevel()
-	{
+		});
+		SceneManager.LoadScene("Level4");
 	}
 }
