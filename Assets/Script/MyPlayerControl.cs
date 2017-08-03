3d76451ad3c7f788c98732c97496e12dc9998ad1
lcyzgdy <MyGuanDY@outlook.com>
2017-08-03 22:45:18 +0800

    Usint colored texture

--- Assets/Script/MyPlayerControl.cs
+++ Assets/Script/MyPlayerControl.cs
@@ -10,6 +10,8 @@ public class MyPlayerControl : MonoBehaviour
 	private new Rigidbody2D rigidbody;
 	private Animator anim;
 	private BoxCollider2D box;
+	private bool grounded;
+	private Vector2 normalVector;
 
 	// Use this for initialization
 	void Start()
@@ -59,18 +61,57 @@ public class MyPlayerControl : MonoBehaviour
 	{
 		var footPosition1 = box.transform.TransformPoint(box.offset + new Vector2(box.size.x, -box.size.y) * 0.5f + Vector2.up);
 		var footPosition2 = box.transform.TransformPoint(box.offset + new Vector2(-box.size.x, -box.size.y) * 0.5f + Vector2.up);
-		var ray1 = Physics2D.Raycast(footPosition1, Vector2.down, 0.2f, whatIsGround);
-		var ray2 = Physics2D.Raycast(footPosition2, Vector2.down, 0.2f, whatIsGround);
+		//var ray1 = Physics2D.Raycast(footPosition1, Vector2.down, 0.2f, whatIsGround);
+		//var ray2 = Physics2D.Raycast(footPosition2, Vector2.down, 0.2f, whatIsGround);
+		var ray1 = Physics2D.Raycast(footPosition1, -normalVector, 0.5f, whatIsGround);
+		var ray2 = Physics2D.Raycast(footPosition2, -normalVector, 0.5f, whatIsGround);
 		Debug.DrawLine(footPosition1, ray1.point, Color.red);
 		Debug.DrawLine(footPosition2, ray2.point, Color.red);
 
-		if (ray1.collider != null || ray2.collider != null)
+		/*if (ray1.collider != null || ray2.collider != null)
 		{
 			return true;
 		}
 		else
 		{
 			return false;
+		}*/
+
+		if (ray1.collider != null || ray2.collider != null)
+		{
+			if (ray1.collider != null && ray2.collider != null)
+			{
+				var d = ray1.distance - ray2.distance;
+				var x = Mathf.Sqrt(box.size.x * box.size.x - d * d);
+				//print(Mathf.Atan(d / x) * Mathf.Rad2Deg);
+				var lineVec = ray1.point - ray2.point;
+				//normalVector = new Vector2(d, x).normalized;
+				normalVector = new Vector2(lineVec.y * -1, lineVec.x) * 10 * transform.localScale.x;
+			}
+			grounded = true;
 		}
+		else
+		{
+			grounded = false;
+			normalVector = Vector2.up;
+		}
+		//Debug.DrawLine(footPosition1, new Vector3(footPosition1.x, footPosition1.y - 0.12f, 0f), Color.red);
+		//Debug.DrawLine(footPosition2, new Vector3(footPosition2.x, footPosition2.y - 0.12f, 0f), Color.red);
+		Debug.DrawLine(footPosition1, ray1.point, Color.red);
+		Debug.DrawLine(footPosition2, ray2.point, Color.red);
+		Debug.DrawRay(transform.position, normalVector, Color.magenta);
+
+		var isStand = Vector2.Angle(Vector2.up, normalVector);
+		//print(isStand);
+		if (!grounded)
+		{
+			transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+		}
+		else if (isStand < 55f)
+		{
+			transform.rotation = Quaternion.Euler(0f, 0f, isStand * (normalVector.x > 0 ? -1 : 1));
+		}
+
+		return grounded;
 	}
 }
