9d6af7ad3905ea71a45c22d1177cbb271b14c2c1
lcyzgdy <MyGuanDY@outlook.com>
2017-10-22 23:18:42 +0800

    Convolution Shader

---
+++ Assets/Script/WebCameraProcess.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WebCameraProcess : MonoBehaviour
+{
+	private WebCamTexture webCameraTexture;
+	[SerializeField] private RenderTexture renderTexture;
+	[SerializeField] private RenderTexture nativeCameraTexture;
+
+	private int width = 1280;
+	private int height = 720;
+	private int cameraFps = 30;
+	[Range(0, 16)]
+	[SerializeField]
+	private int downSample = 0;
+
+	private Material[] materials;
+	[SerializeField] private Shader[] shader;
+
+	//[Range(0.0f, 1.0f)]
+	//[SerializeField]
+	//private float grayThreshold = 0.5f;
+	[SerializeField] private Texture2D skinTexture;
+
+	private void Start()
+	{
+		materials = (from i in shader
+					 where i != null
+					 select GenerateMaterial(i)).ToArray();
+
+		Application.RequestUserAuthorization(UserAuthorization.WebCam);
+		if (!Application.HasUserAuthorization(UserAuthorization.WebCam)) return;
+		WebCamDevice[] devices = WebCamTexture.devices;
+		webCameraTexture = new WebCamTexture(devices[0].name, width, height, cameraFps);
+		webCameraTexture.Play();
+
+		InvokeRepeating("CameraUpdate", 0, 1 / webCameraTexture.requestedFPS);
+	}
+
+	private void CameraUpdate()
+	{
+		if (!webCameraTexture.isPlaying) return;
+		//materials[0].SetFloat("_GrayThreshold", grayThreshold);
+		//materials[0].SetTexture("_SkinTex", skinTexture);
+		//var rt = RenderTexture.GetTemporary(width, height, 0);
+		Graphics.Blit(webCameraTexture, nativeCameraTexture);
+		Graphics.Blit(webCameraTexture, renderTexture, materials[0]);
+		//Graphics.Blit(webCameraTexture, renderTexture);
+	}
+
+	private Material GenerateMaterial(Shader shader)
+	{
+		if (shader == null)
+		{
+			return null;
+		}
+		//需要判断shader是否支持  
+		if (shader.isSupported == false)
+		{
+			return null;
+		}
+
+		Material material = new Material(shader)
+		{
+			hideFlags = HideFlags.DontSave
+		};
+		if (material)
+		{
+			return material;
+		}
+
+		return null;
+	}
+}
---
+++ Assets/Shader/Convolution.shader
@@ -0,0 +1,55 @@
+﻿Shader "Custom/Convolution3x3"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float3x3 _Kernel3x3;
+			float4x4 _Kernel4x4;
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				// just invert the colors
+				col = 1 - col;
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
